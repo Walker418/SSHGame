77bb7d12cf3b808390553369eaee101e014fe600
Yoshioka <>
2019-01-21 15:55:07 +0900

    勝敗

--- SSHGame.php
+++ SSHGame.php
@@ -5,7 +5,7 @@
 		#select{
 			text-align: center
 		}
-		button{
+		#numbuttom{
 			height:64px;
 			width:64px;
 			font-size: 32pt;
@@ -51,22 +51,55 @@
 		}
 	}
 	fclose($fp);
+	
+	
+	if(
+	$numbers["1"]=="○"&&$numbers["2"]=="○"&&$numbers["3"]=="○"||
+	$numbers["4"]=="○"&&$numbers["5"]=="○"&&$numbers["6"]=="○"||
+	$numbers["7"]=="○"&&$numbers["8"]=="○"&&$numbers["9"]=="○"||
+	$numbers["1"]=="○"&&$numbers["4"]=="○"&&$numbers["7"]=="○"||
+	$numbers["2"]=="○"&&$numbers["5"]=="○"&&$numbers["8"]=="○"||
+	$numbers["3"]=="○"&&$numbers["6"]=="○"&&$numbers["9"]=="○"||
+	$numbers["1"]=="○"&&$numbers["5"]=="○"&&$numbers["9"]=="○"||
+	$numbers["3"]=="○"&&$numbers["5"]=="○"&&$numbers["7"]=="○"
+	)
+	{
+		echo "<br>○の勝利";
+	}
+	
+	if(
+	$numbers["1"]=="×"&&$numbers["2"]=="×"&&$numbers["3"]=="×"||
+	$numbers["4"]=="×"&&$numbers["5"]=="×"&&$numbers["6"]=="×"||
+	$numbers["7"]=="×"&&$numbers["8"]=="×"&&$numbers["9"]=="×"||
+	$numbers["1"]=="×"&&$numbers["4"]=="×"&&$numbers["7"]=="×"||
+	$numbers["2"]=="×"&&$numbers["5"]=="×"&&$numbers["8"]=="×"||
+	$numbers["3"]=="×"&&$numbers["6"]=="×"&&$numbers["9"]=="×"||
+	$numbers["1"]=="×"&&$numbers["5"]=="×"&&$numbers["9"]=="×"||
+	$numbers["3"]=="×"&&$numbers["5"]=="×"&&$numbers["7"]=="×"
+	)
+	{
+		echo "<br>×の勝利";
+	}
+	
 	?>
 		
 		<form action="get.php" id="select">
-			<button type="submit" name="uint" value="1"><?php echo $numbers["1"]; ?></button>
-			<button type="submit" name="uint" value="2"><?php echo $numbers["2"]; ?></button>
-			<button type="submit" name="uint" value="3"><?php echo $numbers["3"]; ?></button><br>
-			<button type="submit" name="uint" value="4"><?php echo $numbers["4"]; ?></button>
-			<button type="submit" name="uint" value="5"><?php echo $numbers["5"]; ?></button>
-			<button type="submit" name="uint" value="6"><?php echo $numbers["6"]; ?></button><br>
-			<button type="submit" name="uint" value="7"><?php echo $numbers["7"]; ?></button>
-			<button type="submit" name="uint" value="8"><?php echo $numbers["8"]; ?></button>
-			<button type="submit" name="uint" value="9"><?php echo $numbers["9"]; ?></button><br>
+			<button type="submit" name="uint" value="1" id="numbuttom"><?php echo $numbers["1"]; ?></button>
+			<button type="submit" name="uint" value="2" id="numbuttom"><?php echo $numbers["2"]; ?></button>
+			<button type="submit" name="uint" value="3" id="numbuttom"><?php echo $numbers["3"]; ?></button><br>
+			<button type="submit" name="uint" value="4" id="numbuttom"><?php echo $numbers["4"]; ?></button>
+			<button type="submit" name="uint" value="5" id="numbuttom"><?php echo $numbers["5"]; ?></button>
+			<button type="submit" name="uint" value="6" id="numbuttom"><?php echo $numbers["6"]; ?></button><br>
+			<button type="submit" name="uint" value="7" id="numbuttom"><?php echo $numbers["7"]; ?></button>
+			<button type="submit" name="uint" value="8" id="numbuttom"><?php echo $numbers["8"]; ?></button>
+			<button type="submit" name="uint" value="9" id="numbuttom"><?php echo $numbers["9"]; ?></button><br>
 		</form>
 		
 		<reset>
-		リセットする時は date.txtを空にしてから リロードすること
+		リセットする時は 下のリセットボタンを押すこと
+		<form action="reset.php">
+		<button>リセット</button>
+		</form>
 		</reset>
 	</body>
 </html>
---
+++ reset.php
@@ -0,0 +1,9 @@
+<?php
+$fp = fopen("date.txt","w");
+
+fclose($fp);
+
+/*○×ゲームに戻る*/
+header("Location:SSHGame.php");
+
+?>
